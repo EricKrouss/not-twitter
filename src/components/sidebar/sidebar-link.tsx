6afff9de875864f12b28e93e5cc46f094228c0b4
j4ckxyz <j4ckxyz@users.noreply.github.com>
2026-05-24 00:19:37 +0100

    style: position count badge further top-right and reduce font size to prevent masking the icon

--- src/components/sidebar/sidebar-link.tsx
+++ src/components/sidebar/sidebar-link.tsx
@@ -29,8 +29,8 @@ export function SidebarLink({
       : badgeCount.toString()
     : '';
   const compactCountBadgeClassName = cn(
-    `absolute -top-1 -right-1 flex h-4 items-center justify-center rounded-full
-     border border-main-background bg-main-accent text-[10px] font-bold leading-4 text-white`,
+    `absolute -top-1.5 -right-1.5 flex h-4 items-center justify-center rounded-full
+     border border-main-background bg-main-accent text-[9px] font-bold leading-none text-white`,
     badgeLabel.length === 1 ? 'w-4' : 'min-w-[18px] px-1'
   );
 
